51c4d230c3b09ecd0e35a96007a43ae55016f1f7
Marko Docevski <mdocevski@gmail.com>
2017-04-21 00:17:26 +0200

    Implemented network call to aquire weather data for skopje

---
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -0,0 +1,102 @@
+package com.example.android.sunshine.app;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.squareup.moshi.Moshi;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import timber.log.Timber;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ForecastFragment extends Fragment {
+    private OkHttpClient okHttpClient;
+    private Moshi moshi;
+
+    ArrayAdapter<String> mForecastAdapter;
+
+    public ForecastFragment() {
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        okHttpClient = ((SunshineApplication) getActivity().getApplication()).getOkHttpClient();
+        moshi = ((SunshineApplication) getActivity().getApplication()).getMoshi();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Create some dummy data for the ListView.  Here's a sample weekly forecast
+        String[] data = {
+                "Mon 6/23 - Sunny - 31/17",
+                "Tue 6/24 - Foggy - 21/8",
+                "Wed 6/25 - Cloudy - 22/17",
+                "Thurs 6/26 - Rainy - 18/11",
+                "Fri 6/27 - Foggy - 21/10",
+                "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+                "Sun 6/29 - Sunny - 20/7"
+        };
+        List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
+
+
+        // Now that we have some dummy forecast data, create an ArrayAdapter.
+        // The ArrayAdapter will take data from a source (like our dummy forecast) and
+        // use it to populate the ListView it's attached to.
+        mForecastAdapter =
+                new ArrayAdapter<>(
+                        getActivity(), // The current context (this activity)
+                        R.layout.list_item_forecast, // The name of the layout ID.
+                        R.id.list_item_forecast_textview, // The ID of the textview to populate.
+                        weekForecast);
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        // Get a reference to the ListView, and attach this adapter to it.
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(mForecastAdapter);
+
+        String getWeatherDataUrl =
+                "http://api.openweathermap.org/data/2.5/forecast/daily?zip=1000,mk&units=metric&mode=json&cnt=7"
+                        + "&appId=" + BuildConfig.OPEN_WEATHER_MAP_API_KEY;
+        okHttpClient.newCall(
+                new Request.Builder()
+                        .get()
+                        .url(getWeatherDataUrl)
+                        .build())
+                .enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+                        Timber.e(e, "Failed to acquire weather data.");
+                    }
+
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+//                            moshi.
+                        Timber.d(response.body().string());
+                    }
+                });
+
+
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/example/android/sunshine/app/SunshineApplication.java
@@ -0,0 +1,75 @@
+package com.example.android.sunshine.app;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.squareup.moshi.Moshi;
+import com.squareup.picasso.Picasso;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import timber.log.Timber;
+
+/**
+ * Created by mdocevski on 20.4.17.
+ */
+
+public class SunshineApplication extends Application {
+    private OkHttpClient okHttpClient;
+    private Moshi moshi;
+    private Picasso picasso;
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BASIC);
+        moshi = new Moshi.Builder()
+                // add the converter factories here
+                .build();
+        okHttpClient = new OkHttpClient.Builder()
+                .addInterceptor(loggingInterceptor)
+                .build();
+        picasso = Picasso.with(getApplicationContext());
+//                new Picasso.Builder(getApplicationContext())
+//                .build();
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        } else {
+            Timber.plant(new CrashReportingTree());
+        }
+    }
+
+    public OkHttpClient getOkHttpClient() {
+        return okHttpClient;
+    }
+
+    public Moshi getMoshi() {
+        return moshi;
+    }
+
+    public Picasso getPicasso() {
+        return picasso;
+    }
+
+    //TODO NOT LEAVE LIKE THIS
+    @SuppressWarnings("all")
+    private class CrashReportingTree extends Timber.Tree {
+        @Override protected void log(int priority, String tag, String message, Throwable t) {
+            if (priority == Log.VERBOSE || priority == Log.DEBUG) {
+                return;
+            }
+//            FakeCrashLibrary.log(priority, tag, message);
+//
+//            if (t != null) {
+//                if (priority == Log.ERROR) {
+//                    FakeCrashLibrary.logError(t);
+//                } else if (priority == Log.WARN) {
+//                    FakeCrashLibrary.logWarning(t);
+//                }
+//            }
+        }
+    }
+}
